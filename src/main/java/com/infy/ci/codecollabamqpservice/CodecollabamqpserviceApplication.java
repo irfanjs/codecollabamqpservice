aeda58021c8d60e18a47916ed0a1d2c897755a6d
Sayed <Irfan.Sayed@Honeywell.com>
2019-07-29 15:51:09 +0530

    first commit

---
+++ src/main/java/com/infy/ci/codecollabamqpservice/CodecollabamqpserviceApplication.java
@@ -0,0 +1,13 @@
+package com.infy.ci.codecollabamqpservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodecollabamqpserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodecollabamqpserviceApplication.class, args);
+	}
+
+}
